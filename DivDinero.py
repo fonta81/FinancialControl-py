98f90099bce32c7f55aa504165dd6a546ff888fd
mteo <cortinamateo211@gmail.com>
2026-06-17 17:28:35 -0500

    OptimizarEstructuraDeDatosConTuplas&CorregirLógicaDeSuma
    - Corrige bug en GuardarSuma() leyendo correctamente el estado previo
    del JSON.
    - Cambia nombre del archivo de guardado a 'Estudio.json'.
    - Añade formato de millares (:,.2f) en la impresión de montos."

--- DivDinero.py
+++ DivDinero.py
@@ -1,139 +1,115 @@
-import json
-import datetime
+#nesesito un programa que sea capaz de dividir el dinero -> guararlo(0 o sum) -> cargarlo
+import datetime; import json
+n = datetime.datetime.now() #dia de hoy
+Hoy = n.strftime('%Y/%m/%d')
 
-n = datetime.datetime.now()
-Hoy = n.strftime('%Y/%m/%d') #para poder imprimir el dia de hoy sin problema 
-
-DatosPrincipales = { #los datos que se usaran durante todo el codigo 
-    'MJugar': 0,
-    'Libertad': 0,
-    'AhorrosLargoPlazo': 0,
-    'Formacion': 0,
-    'Basicos': 0,
-    'Donativos': 0
+Montos = { #datos a guardar
+    'El monto para Jugar es de: ': (0.0,0.1),
+    'El monto para Libertad financiera es de: ': (0.0,0.1),
+    'El monto para los ahorros a largo plazo es de: ': (0.0,0.1),
+    'El monto para la formacion es de: ': (0.0,0.1),
+    'El monto para los basicos es de: ': (0.0,0.5),
+    'El monto para las donaciones es de: ': (0.0,0.1)
 }
-def cargar():
-    global DatosPrincipales
-    try:
-        with open("DinerosDiv.json", "r", encoding="utf-8") as archivo: #carga los datos
-            datos_cargados = json.load(archivo)
-            
-            for clave in DatosPrincipales.keys(): #remplaza los datos del diccionario actual con los que cargo
-                DatosPrincipales[clave] = datos_cargados.get(clave, 0)
-                
-        print("Datos cargados correctamente en el sistema.")
 
-    except: print('No se pudo cargar correctamente los datos')#en caso de error 
-
-
-def Guardar():
-    try:        
-        with open("DinerosDiv.json", "w", encoding="utf-8") as archivo: #guarda los archivos desde 0
-            json.dump(DatosPrincipales, archivo, indent=4, ensure_ascii=False)
-        print('Los datos se guardaron correctamente')
-    except: print('no se pudo guardar correctamente') #en caso de error
-
-def SumGuar():
-    global DatosPrincipales 
-    try:
-        with open("DinerosDiv.json", "r", encoding="utf-8") as archivo: #carga los archivos
-            DatosPrincipales = json.load(archivo)
-                
-        for clave in DatosPrincipales.keys():
-            DatosPrincipales[clave] += DatosPrincipales.get(clave, 0) #le suma los actuales a los q ya estaban 
-            
-        Guardar() #los guarda
-        print('Los datos se actualizaron correctamente')
-    except: print('no se pudo guardar correctamente') #en caso de error
-
-def main():
-    cargar() 
+def DivDinero():
+    global Montos
     while True:
-        try: mon = float(input('Ingresa el monto de $: ')); break #dato correcto = salir del bucle
-        except: print('ingrese un dato valido')#dato que no sea float = repetir la pregunta
-
-    global DatosPrincipales #se halla el valor de cada uno segun lo que nos dio el usuario 
-    DatosPrincipales['MJugar'] = mon * 0.1
-    DatosPrincipales['Libertad'] = mon * 0.1
-    DatosPrincipales['AhorrosLargoPlazo'] = mon * 0.1
-    DatosPrincipales['Formacion'] = mon * 0.1
-    DatosPrincipales['Basicos'] = mon * 0.5
-    DatosPrincipales['Donativos'] = mon * 0.1
-
-    print('\n') # se imprimen los datos de la division al usuario 
-    print(Hoy)
+        try: MontoaDividir = int(input('Dijite el monto a dividir: ')); break # preguntando el monto a dividir
+        except: print('inrgese un valor valido')
+    for nombre in Montos: #dando valores 
+        div = Montos.get(nombre)[1]  
+        valor = MontoaDividir * div
+        Montos[nombre] = (valor,div)
+    print('\n\n')    
+    print(f'Total = {MontoaDividir:,.0f} / {MontoaDividir}')
+    print(Hoy) #mostrando los valores    
     print('='*60)
-
-    print(f"El monto para Jugar es de: {DatosPrincipales['MJugar']:.2f}")
-    print(f"El monto para Libertad financiera es de: {DatosPrincipales['Libertad']:.2f}")
-    print(f"El monto para los ahorros a largo plazo es de: {DatosPrincipales['AhorrosLargoPlazo']:.2f}")
-    print(f"El monto para la formacion es de: {DatosPrincipales['Formacion']:.2f}")
-    print(f"El monto para los basicos es de: {DatosPrincipales['Basicos']:.2f}")
-    print(f"El monto para las donaciones es de: {DatosPrincipales['Donativos']:.2f}")
+    for nombre in Montos:        
+        print(f'{nombre} {Montos.get(nombre)[0]:,.2f}')
+    print('='*60)
     
-    print('='*60)
+def Guardar0():
+    try:
+        with open('Estudio.json', 'w', encoding='utf-8') as archivo: #guardando datos desde 0
+            json.dump(Montos,archivo,indent=4,ensure_ascii=False)
+        print('Los datos se guardaron correctamente')
+    except: print('no se guardaron correctamente')
+
+def GuardarSuma():
+    global Montos
+    try:
+        with open('Estudio.json', 'r', encoding='utf-8') as archivo: #cargando los datos
+            MontosC = json.load(archivo)
+            for key in Montos:
+                ValorActual = Montos.get(key)[0] #lo separamos de los dato 
+                Porcentaje = Montos.get(key)[1] # lo separamos de los dato 
+                valor = ValorActual + MontosC.get(key)[0] #hallamos la suma
+                Montos[key] = (valor,Porcentaje) # y lo volvemos a guardar
+        Guardar0()
+        print('los datos se guardaron correctamente')
+    except: print('no se pudo guardar correctamente')
+
+def Cargar():
+    global Montos
+    try:
+        with open('Estudio.json', 'r', encoding='utf-8') as archivo: #cargando los datos
+            Montos = json.load(archivo)                    
+    except: print('no se pudo cargar correctamente')
+
+def EleccionGuar():    
+    print('1. Guardar y sumar datos actuales o 2.Guardar y eliminar datos anteriores') #se pregunta que funcion de guardado se usara 
+    while True:
+        try: Tguar = int(input('Elige: ')); break
+        except: print('ingrese un valor valido')        
+    if Tguar in (1,2): # dentro de parametros?
+        if Tguar == 1: #si es guardado normal es decir: sumguar
+            GuardarSuma()
+        elif Tguar == 2: 
+            print('Estas seguro?\n1.si  2.no') # se verifica que el wey esta seguro 
+            while True:
+                try: Seguro = int(input('Elige: ')); break
+                except: print('ingrese un valor valido')
+            if Seguro in (1,2):
+                if Seguro == 1: # si la respuesta es si entonces se guarda
+                    Guardar0()
+                elif Seguro == 2: # si no lo esta se usa return para salir 
+                    return
+            else: print('Ingrese un valor valido') # si pone un valor afuera de los parametros
+    else: print('ingrese un dato valido')
 
 def MostrarDatos():
+    print(Hoy) #mostrando los valores    
+    for nombre in Montos:        
+        print(f'{nombre} {Montos.get(nombre)[0]:,.2f}')        
 
-    print('\n') # se imprimen los datos de la division al usuario 
-    print(Hoy)
-    print('='*60)
 
-    print(f"El monto para Jugar es de: {DatosPrincipales['MJugar']:.2f}")
-    print(f"El monto para Libertad financiera es de: {DatosPrincipales['Libertad']:.2f}")
-    print(f"El monto para los ahorros a largo plazo es de: {DatosPrincipales['AhorrosLargoPlazo']:.2f}")
-    print(f"El monto para la formacion es de: {DatosPrincipales['Formacion']:.2f}")
-    print(f"El monto para los basicos es de: {DatosPrincipales['Basicos']:.2f}")
-    print(f"El monto para las donaciones es de: {DatosPrincipales['Donativos']:.2f}")
-    
-    print('='*60)
-
-def menu():
+def main():
     while True:
-        print('\n')
         print('='*60)
-        print('1.Guardar')
-        print('2.Cargar')
-        print('3.Mostrar datos actuales')
-        print('4.main')
-        print('5.Salir')
+        print('1.Dividir dinero')
+        print('2.guardar')
+        print('3.cargar')
+        print('4.Mostrar datos actuales')
+        print('5.salir')
         while True:
-            try: elg = int(input('Elige: ')); break
-            except: print('ingrese un valor valido')
-        
-        if elg in (range(1,6)):
-            if elg == 1:
-                print('1. Guardar y sumar datos actuales o 2.Guardar y eliminar datos anteriores') #se pregunta que funcion de guardado se usara 
-                while True:
-                    try: Tguar = int(input('Elige: ')); break
-                    except: print('ingrese un valor valido')
-                    
-                if Tguar in (1,2): # dentro de parametros?
-
-                    if Tguar == 1: #si es guardado normal es decir: sumguar
-                        SumGuar()
-
-                    elif Tguar == 2: 
-                        print('Estas seguro?\n1.si  2.no') # se verifica que el wey esta seguro 
-                        while True:
-                            try: Seguro = int(input('Elige: ')); break
-                            except: print('ingrese un valor valido')
-                        if Seguro in (1,2):
-                            if Seguro == 1: # si la respuesta es si entonces se guarda
-                                Guardar()
-                            elif Seguro == 2: # si no lo esta se usa return para salir 
-                                return
-                        else: print('Ingrese un valor valido') # si pone un valor afuera de los parametros
-
-                else: print('Ingrese un valor valido')            
-            elif elg == 2:
-                cargar()
-            elif elg == 3:
+            try: elige = int(input('Elige: ')); break
+            except: print('inrgese un valor valido')
+        if elige in (range(1,6)):
+            if elige == 1:
+                print('='*60)
+                DivDinero()
+            elif elige == 2:
+                print('='*60)
+                EleccionGuar()
+            elif elige == 3:
+                print('='*60)
+                Cargar()
+            elif elige == 4:
+                print('='*60)
                 MostrarDatos()
-            elif elg == 4:
-                main()
-            else: break
+            else:
+                break
+        else: print('ingrese un valor valido')
 
-
-
-menu()
+main()
