d4d1d95fbd08ec902b113b249aeec0e1bb1bf2f2
mteo <cortinamateo211@gmail.com>
2026-06-26 21:34:29 -0500

    Agrego: gitnore && Cambio: Divdinero.py
    -se agrego gitnore
    -DivDinero se restrocturo x medio de class

--- DivDinero.py
+++ DivDinero.py
@@ -1,115 +1,125 @@
-#nesesito un programa que sea capaz de dividir el dinero -> guararlo(0 o sum) -> cargarlo
-import datetime; import json
-n = datetime.datetime.now() #dia de hoy
-Hoy = n.strftime('%Y/%m/%d')
+import json
+import datetime
 
-Montos = { #datos a guardar
-    'El monto para Jugar es de: ': (0.0,0.1),
-    'El monto para Libertad financiera es de: ': (0.0,0.1),
-    'El monto para los ahorros a largo plazo es de: ': (0.0,0.1),
-    'El monto para la formacion es de: ': (0.0,0.1),
-    'El monto para los basicos es de: ': (0.0,0.5),
-    'El monto para las donaciones es de: ': (0.0,0.1)
+
+now = datetime.datetime.now()
+Hoy = now.strftime("%Y/%m/%d")
+montoactualdiv = 0
+DivDinerosDicc = {
+    "Ahorros Largo Plazo": (0, 0.1),
+    "formacion": (0, 0.1),
+    "Basicos": (0, 0.5),
+    "Donativos": (0, 0.1),
+    "Jugar": (0, 0.1),
+    "Libertad Financiera": (0, 0.1),
 }
 
-def DivDinero():
-    global Montos
-    while True:
-        try: MontoaDividir = int(input('Dijite el monto a dividir: ')); break # preguntando el monto a dividir
-        except: print('inrgese un valor valido')
-    for nombre in Montos: #dando valores 
-        div = Montos.get(nombre)[1]  
-        valor = MontoaDividir * div
-        Montos[nombre] = (valor,div)
-    print('\n\n')    
-    print(f'Total = {MontoaDividir:,.0f} / {MontoaDividir}')
-    print(Hoy) #mostrando los valores    
-    print('='*60)
-    for nombre in Montos:        
-        print(f'{nombre} {Montos.get(nombre)[0]:,.2f}')
-    print('='*60)
-    
-def Guardar0():
-    try:
-        with open('Ahorros.json', 'w', encoding='utf-8') as archivo: #guardando datos desde 0
-            json.dump(Montos,archivo,indent=4,ensure_ascii=False)
-        print('Los datos se guardaron correctamente')
-    except: print('no se guardaron correctamente')
 
-def GuardarSuma():
-    global Montos
-    try:
-        with open('Ahorros.json', 'r', encoding='utf-8') as archivo: #cargando los datos
-            MontosC = json.load(archivo)
-            for key in Montos:
-                ValorActual = Montos.get(key)[0] #lo separamos de los dato 
-                Porcentaje = Montos.get(key)[1] # lo separamos de los dato 
-                valor = ValorActual + MontosC.get(key)[0] #hallamos la suma
-                Montos[key] = (valor,Porcentaje) # y lo volvemos a guardar
-        Guardar0()
-        print('los datos se guardaron correctamente')
-    except: print('no se pudo guardar correctamente')
+class DinerosClass:
+    def MontoDiv(self, MontoActualDividir):
+        global DivDinerosDicc
+        global montoactualdiv
+        montoactualdiv = MontoActualDividir
+        for NombreDeMontos, Valores in DivDinerosDicc.items():
+            DivDinerosDicc[NombreDeMontos] = (
+                MontoActualDividir * Valores[1],
+                Valores[1],
+            )
+        print("Los valores entonces seran:")
+        for NombreDeMontos, Dinero in DivDinerosDicc.items():
+            print(f"El monto para {NombreDeMontos} = {Dinero[0]:,.2f}")
 
-def Cargar():
-    global Montos
-    try:
-        with open('Ahorros.json', 'r', encoding='utf-8') as archivo: #cargando los datos
-            Montos = json.load(archivo)                    
-    except: print('no se pudo cargar correctamente')
+    def MostrarActuales(self):
+        print(f"{montoactualdiv}/{montoactualdiv:,}")
+        print(Hoy)
+        for NombreDeMontos, Dinero in DivDinerosDicc.items():
+            print(f"El monto para {NombreDeMontos} = {Dinero[0]:,.2f}")
 
-def EleccionGuar():    
-    print('1. Guardar y sumar datos actuales o 2.Guardar y eliminar datos anteriores') #se pregunta que funcion de guardado se usara 
-    while True:
-        try: Tguar = int(input('Elige: ')); break
-        except: print('ingrese un valor valido')        
-    if Tguar in (1,2): # dentro de parametros?
-        if Tguar == 1: #si es guardado normal es decir: sumguar
-            GuardarSuma()
-        elif Tguar == 2: 
-            print('Estas seguro?\n1.si  2.no') # se verifica que el wey esta seguro 
-            while True:
-                try: Seguro = int(input('Elige: ')); break
-                except: print('ingrese un valor valido')
-            if Seguro in (1,2):
-                if Seguro == 1: # si la respuesta es si entonces se guarda
-                    Guardar0()
-                elif Seguro == 2: # si no lo esta se usa return para salir 
-                    return
-            else: print('Ingrese un valor valido') # si pone un valor afuera de los parametros
-    else: print('ingrese un dato valido')
 
-def MostrarDatos():
-    print(Hoy) #mostrando los valores    
-    for nombre in Montos:        
-        print(f'{nombre} {Montos.get(nombre)[0]:,.2f}')        
+class ModificarArchivo:
+    def cargar(self):
+        global DivDinerosDicc
+        try:
+            with open("DivDinero.json", "r", encoding="utf-8") as archivo:
+                DivDinerosDicc = json.load(archivo)
+                print("Se cargaron correctamente los datos")
+
+        except Exception as e:
+            print(f"no se pudo cargar correctamente\n error: {e}")
+
+    def Guardar(self):
+        global DivDinerosDicc
+        try:
+            with open("DivDinero.json", "w", encoding="utf-8") as archivo:
+                json.dump(DivDinerosDicc, archivo, indent=4, ensure_ascii=False)
+                print("Guardo correctamente")
+        except Exception as e:
+            print(f"no se pudo guardar correctamente\n error: {e}")
+
+    def SumGuar(self):
+        global DivDinerosDicc
+        try:
+            with open("DivDinero.json", "r", encoding="utf-8") as archivo:
+                DatosCargados = json.load(archivo)
+                for nombre, cantidad in DivDinerosDicc.items():
+                    DivDinerosDicc[nombre] = (
+                        cantidad[0] + DatosCargados.get(nombre)[0],
+                        cantidad[1],
+                    )
+            self.Guardar()
+
+        except Exception as e:
+            print(f"no se pudo cargar correctamente\n error: {e}")
+
+    def Eleccion(self):
+        GuardarCargar = input("1.Guardar\n2.cargar\nElige:")
+        if GuardarCargar == "2":
+            self.cargar()
+        elif GuardarCargar == "1":
+            EligeSum = input(
+                "Quieres sumar los datos actuales con los guardados?:\n1.si\n2.no\nElige: "
+            )
+            if EligeSum == "1":
+                self.SumGuar()
+            elif EligeSum == "2":
+                self.Guardar()
+            else:
+                print("ingresa un valor valido")
+        else:
+            print("Digite un valor valido")
 
 
 def main():
+    Din = DinerosClass()
+    Modi = ModificarArchivo()
     while True:
-        print('='*60)
-        print('1.Dividir dinero')
-        print('2.guardar')
-        print('3.cargar')
-        print('4.Mostrar datos actuales')
-        print('5.salir')
-        while True:
-            try: elige = int(input('Elige: ')); break
-            except: print('inrgese un valor valido')
-        if elige in (range(1,6)):
-            if elige == 1:
-                print('='*60)
-                DivDinero()
-            elif elige == 2:
-                print('='*60)
-                EleccionGuar()
-            elif elige == 3:
-                print('='*60)
-                Cargar()
-            elif elige == 4:
-                print('='*60)
-                MostrarDatos()
-            else:
-                break
-        else: print('ingrese un valor valido')
+        print("=" * 60)
+        print("1.Dividir dinero")
+        print("2.Guardar o cargar")
+        print("3.Mostrar diviciones actuales")
+        print("4.Salir")
+        print("=" * 60)
+        Elige = input("Elige: ")
 
-main()
+        if Elige == "4":
+            print("Saliendo...")
+            break
+        elif Elige == "1":
+            print("\n")
+            print("=" * 60)
+            Din.MontoDiv(int(input("Digite el monto a dividir: ")))
+
+        elif Elige == "2":
+            print("\n")
+            print("=" * 60)
+            Modi.Eleccion()
+        elif Elige == "3":
+            print("\n")
+            print("=" * 60)
+            Din.MostrarActuales()
+        else:
+            print("Ingrese un valor valido")
+
+
+if __name__ == "__main__":
+    main()
